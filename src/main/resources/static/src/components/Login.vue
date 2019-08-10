e7e6b44e80abeb247314ad7198ca7cc8d57ed154
Maxp <1061231908@qq.com>
2019-08-10 17:12:52 +0800

    加上前端代码

---
+++ src/main/resources/static/src/components/Login.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div class="">{{title}}</div>
+        <div><input placeholder="用户名" /></div>
+        <div><input placeholder="密码" /></div>
+        <div><button>登录</button></div>
+        <div><button>注册</button></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      title: '办公考勤ERP系统'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
